87a2c52a07a9814b92b49200ca549ccacb4f704f
silversthem <silversthem@gmail.com>
2016-02-12 17:54:53 +0100

    Slight change in architecture, created all core files, just have to fill them

--- core/helpers.hpp
+++ core/helpers.hpp
@@ -31,17 +31,19 @@ Oriented::Segment segment(float angle,float length)
 	return s;
 }
 
+Interval_Member interval_member(float value,bool excluded = false)
+{
+	Interval_Member m;
+	m.Value = value;
+	m.Excluded = excluded;
+	return m;
+}
+
 Interval interval(float start,float end,bool startEx = false,bool endEx = false)
 {
 	Interval I;
-	Interval_Member Istart;
-	Istart.Value = start;
-	Istart.Excluded = startEx;
-	I.Start = Istart;
-	Interval_Member Iend;
-	Iend.Value = end;
-	Iend.Excluded = endEx;
-	I.End = Iend;
+	I.Start = interval_member(start,startEx);
+	I.End = interval_member(end,endEx);
 	return I;
 }
 
